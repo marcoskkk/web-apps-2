6b8b1a5cfed2c2ae1e412bde72d67b2a11bb0f54
marcoskkk <marcos.kashima@kellogg.northwestern.edu>
2022-02-15 17:07:16 +0000

    save during class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,19 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    # .find(123) is the same as .where({ id: 123 })[0]
+    @post = Post.find(params["id"])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to "/posts"
+  end
+
 end
